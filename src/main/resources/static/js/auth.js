fc6f487ff00ffda60f307ace6b4272257077fa17
meoaa <wsxqaz789456@naver.com>
2025-07-28 15:31:23 +0900

    thymeleaf layout

---
+++ src/main/resources/static/js/auth.js
@@ -0,0 +1,21 @@
+export function setupLogout(){
+    const logoutBtn = document.querySelector(".logout");
+       if(logoutBtn){
+            logoutBtn.addEventListener("click",()=>{
+                  $.ajax({
+                    url : "/api/auth/logout",
+                    method : "post",
+                    success : function(response){
+                      location.href ="/login?logout";
+                    },
+                    error: function(error){
+                      console.log(error);
+                    }
+                  })
+                })
+            }
+       }
+
+document.addEventListener("DOMContentLoaded", function() {
+  setupLogout();
+});
---
+++ src/main/resources/static/js/common.js
@@ -0,0 +1,5 @@
+import{ setupLogout } from './auth.js'
+
+document.addEventListener("DOMContentLoaded", function() {
+  setupLogout();
+});
